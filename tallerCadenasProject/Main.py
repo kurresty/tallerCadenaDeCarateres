5256e365f89a7b681cce712fe4dc4aa9f75056e8
Dayana Lopez <dajhanalopez29@gmail.com>
2024-09-21 18:27:28 -0500

    Se agrega tallerCadenasProject/Main.py

---
+++ tallerCadenasProject/Main.py
@@ -0,0 +1,24 @@
+#Punto 1
+name = "Luis"
+age = 27
+favouriteFood = "Pasta con salsa Boloñesa"
+presentacion = f"Hola! Mi nombre es {name}. Yo tengo {age} años, y mi comida favorita es {favouriteFood}"
+print(presentacion)
+
+
+
+#Punto 2
+nombre = input("Hola ingresa tu nombre completo: ")
+contarNombre = len(nombre.replace(" ", ""))
+saludo = f"Hola, {nombre}! Tu nombre tiene {contarNombre} letras"
+print(saludo)
+
+#Punto3
+increaseSalesPercent = 12.93720081
+revenueGrowthPercent = 18.33206078
+mensaje = (f"Las ventas de la empresa láctea aumentaron un {increaseSalesPercent:.2f}% y "
+           f"los ingresos crecieron un {revenueGrowthPercent:.2f}%.")
+print(mensaje)
+
+
+#Punto 4
